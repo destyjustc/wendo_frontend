34c1c7e965d0ee7e0a929e562ab6c5b6a68fc670
Kevin Zhang <kevinzhang@fortinet.com>
2018-04-24 09:47:40 -0700

    prepare for deploy

--- src/pages/dashboard/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'dashboard-page',
-    templateUrl: 'dashboard.html'
-})
-export class DashboardPage {
-    constructor() {}
-}
